6dda6c8a2848e15c892e826c3bcb9f1fe552602e
Ben Longbons <b.r.longbons@gmail.com>
2013-09-12 20:37:36 -0700

    Add copyright stuff

--- input-impl.c
+++ input-impl.c
@@ -1,3 +1,18 @@
+// Nacreous EggShell is a shell with no major limitations nor features.
+// Copyright (C) 2013  Ben Longbons <b.r.longbons@gmail.com>
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "input-impl.h"
 
 #include <string.h>
